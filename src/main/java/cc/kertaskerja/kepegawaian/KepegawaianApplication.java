f9fc0fb3d3e82bfac3e5585dcef268af04dde77d
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2026-06-17 12:14:50 +0700

    initial commit

---
+++ src/main/java/cc/kertaskerja/kepegawaian/KepegawaianApplication.java
@@ -0,0 +1,13 @@
+package cc.kertaskerja.kepegawaian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KepegawaianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KepegawaianApplication.class, args);
+	}
+
+}
